0a9c1166ff5b1b5f02ff79c607941f3fd4fac717
Elnaz <elnazrezaee80@gmail.com>
2020-11-23 20:35:22 +0330

    c tags added

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,age;
+    scanf("%d",&a);
+    printf("hello");
+    printf("a=%d",a);
+    printf("how old are you");
+    scanf("%d",&age);
+    return0;
+}
